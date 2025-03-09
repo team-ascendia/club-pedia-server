6d950d912bdd33f746f53c8c1ee05e44c0121ffc
rrosiee <offbeat1020@naver.com>
2025-03-09 16:58:06 +0900

    feat: 장리 기준 필터링 repository

--- src/main/java/clubpedia/in/net/clubpedia/club/repository/ClubCustomRepository.java
+++ src/main/java/clubpedia/in/net/clubpedia/club/repository/ClubCustomRepository.java
@@ -1,11 +1,8 @@
 package clubpedia.in.net.clubpedia.club.repository;
 
 import clubpedia.in.net.clubpedia.club.domain.Club;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 public interface ClubCustomRepository {
-//    List<Club> findAllByGenreIds(List<Integer> genreIds);
+    List<Club> findAllByGenreIds(List<Long> genreIds);
 }
--- src/main/java/clubpedia/in/net/clubpedia/club/repository/ClubCustomRepositoryImpl.java
+++ src/main/java/clubpedia/in/net/clubpedia/club/repository/ClubCustomRepositoryImpl.java
@@ -1,6 +1,8 @@
 package clubpedia.in.net.clubpedia.club.repository;
 
 import clubpedia.in.net.clubpedia.club.domain.Club;
+import clubpedia.in.net.clubpedia.club.domain.QClub;
+import clubpedia.in.net.clubpedia.genre.domain.QGenre;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -9,10 +11,19 @@ import java.util.List;
 
 @Repository
 public class ClubCustomRepositoryImpl implements ClubCustomRepository {
-    @Autowired
-    JPAQueryFactory queryFactory;
 
-//    public List<Club> findAllByGenreIds(List<Integer> genreIds) {
-//        return queryFactory.selectFrom(Club)
-//    }
+    @Autowired
+    private JPAQueryFactory queryFactory;
+
+    @Override
+    public List<Club> findAllByGenreIds(List<Long> genreIds) {
+        QClub club = QClub.club;
+        QGenre genre = QGenre.genre;
+
+        return queryFactory
+                .selectFrom(club)
+                .join(club.genres, genre)
+                .where(genre.id.in(genreIds))
+                .fetch();
+    }
 }
